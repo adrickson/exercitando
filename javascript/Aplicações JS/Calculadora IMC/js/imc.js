1cb1c4bc4c740925a6bea2d406806e2c42cf4b1d
adrickson <144737887+adrickson@users.noreply.github.com>
2024-02-05 22:06:30 -0300

    adicionando situacao de peso ao js

--- javascript/Aplicações JS/Calculadora IMC/js/imc.js
+++ javascript/Aplicações JS/Calculadora IMC/js/imc.js
@@ -4,10 +4,13 @@ let cx_idade = document.getElementById('idade')
 let cx_peso = document.getElementById('peso')
 let cx_altura = document.getElementById('altura')
 let cx_imc = document.getElementById('imc')
+let definicao = document.getElementById('definicao')
 
 let btn_enviar = document.getElementById('btnEnviar')
 
 btn_enviar.addEventListener('click', function (e) {
+
+    //CALCULO IMC
 
     let nome = cx_nome.value
     let idade = Number(cx_idade.value)
@@ -16,10 +19,30 @@ btn_enviar.addEventListener('click', function (e) {
 
     let soma_imc = (peso / (altura * altura)).toFixed(2)
 
+    // SITUAÇÃO DA PESSOA ATRAVÉS DO RESULTADO DO IMC
+
+    let sit = situacaoPeso(soma_imc)
+
     // VARIAVEIS DE RETORNO
 
     cx_imc.value = soma_imc
+    definicao.innerHTML = sit
 
     // PREVENIR O COMPORTAMENTO PADRÃO DO FORMULÁRIO DE ENVIAR DADOS
     e.preventDefault()
 })
+
+function situacaoPeso(soma_imc) {
+    let situacao = ''
+
+    if (soma_imc < 18.5) {
+        situacao = 'Baixo Peso'
+    } else if (soma_imc >= 18.5 && soma_imc <= 24.9) {
+        situacao = 'Peso Normal'
+    } else if (soma_imc >= 25 && soma_imc <= 29.9){
+        situacao = 'Acima do Peso'
+    }
+
+    return situacao
+
+}
