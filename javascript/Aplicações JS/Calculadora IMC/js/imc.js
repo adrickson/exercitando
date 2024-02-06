8db76d46609b378f853427e6089f7f9815897f64
adrickson <144737887+adrickson@users.noreply.github.com>
2024-02-06 00:30:51 -0300

    calculadora imc funcionando

--- javascript/Aplicações JS/Calculadora IMC/js/imc.js
+++ javascript/Aplicações JS/Calculadora IMC/js/imc.js
@@ -4,7 +4,9 @@ let cx_idade = document.getElementById('idade')
 let cx_peso = document.getElementById('peso')
 let cx_altura = document.getElementById('altura')
 let cx_imc = document.getElementById('imc')
+
 let definicao = document.getElementById('definicao')
+let dadosP = document.querySelectorAll('.pessoa')
 
 let btn_enviar = document.getElementById('btnEnviar')
 
@@ -23,10 +25,27 @@ btn_enviar.addEventListener('click', function (e) {
 
     let sit = situacaoPeso(soma_imc)
 
+    // CRIANDO OBJ PESSOA
+
+    let pessoa = {
+        nome : nome,
+        idade : idade,
+        peso : peso,
+        altura: altura,
+        imc: soma_imc,
+        sit: sit,
+    }
+
     // VARIAVEIS DE RETORNO
 
     cx_imc.value = soma_imc
     definicao.innerHTML = sit
+    dadosP[0].innerHTML = `Nome: <strong>${pessoa.nome}</strong>`
+    dadosP[1].innerHTML = `Idade: <strong>${pessoa.idade}</strong>`
+    dadosP[2].innerHTML = `Peso: <strong>${pessoa.peso}Kg</strong>`
+    dadosP[3].innerHTML = `Altura: <strong>${pessoa.altura}m</strong>`
+    dadosP[4].innerHTML = `IMC: <strong>${pessoa.imc}</strong>`
+    dadosP[5].innerHTML = `Situação Atual: <strong>${pessoa.sit}</strong>`
 
     // PREVENIR O COMPORTAMENTO PADRÃO DO FORMULÁRIO DE ENVIAR DADOS
     e.preventDefault()
@@ -41,6 +60,14 @@ function situacaoPeso(soma_imc) {
         situacao = 'Peso Normal'
     } else if (soma_imc >= 25 && soma_imc <= 29.9){
         situacao = 'Acima do Peso'
+    } else if (soma_imc >= 30 && soma_imc <= 34.9) {
+        situacao = 'Obesidade Grau 1'
+    } else if (soma_imc >= 35 && soma_imc <= 40) {
+        situacao = 'Obesidade Grau 2'
+    } else if(soma_imc >= 40) {
+        situacao = 'Obesidade Grau 3'
+    } else {
+        situacao = 'Informe seu peso corretamente'
     }
 
     return situacao
