22fbbea9315bdc5ba36dd940a456c7bf6a1deba9
adrickson <144737887+adrickson@users.noreply.github.com>
2024-02-28 21:14:55 -0300

    entendendo o conceito de classe

---
+++ JS-POO/aula01/js/main.js
@@ -0,0 +1,31 @@
+
+class Pessoa
+{
+    // atributos = variáveis ou características 
+
+    nome
+    idade
+    peso
+
+    //métodos = funções ou comportamentos 
+
+
+}
+
+// criar novo objeto a partir da classe, instanciar
+
+let pessoa1 = new Pessoa()
+let pessoa2 = new Pessoa()
+
+//Atribuir valores para os atributos de um objeto
+
+pessoa1.nome = 'Adrickson'
+pessoa1.idade = 19
+pessoa1.peso = 70.5
+
+pessoa2.nome = 'Paula'
+pessoa2.idade = 18
+pessoa2.peso = 60.3
+
+console.log(pessoa1)
+console.log(pessoa2)
